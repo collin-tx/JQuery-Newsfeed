68702c28c1e587e9e4150e7bc94cac36eb137552
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-03-12 13:57:43 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+const $odd = $('a:odd');
+const $ecureLinks = $('a[href^="https://"]');
+const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $('<label><input type="checkbox">Allow PDF downloads</label>');
 
+
+$ecureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
+
+$odd.css('backgroundColor', 'lightGrey');
+
+$ecureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+$pdfs.on('click', function(e){
+  if ($(':checked').length === 0){
+    e.preventDefault();
+    alert('Please check the box to allow PDF downloads.');
+  }
+});
+
+$('#links').append($pdfCheckbox);
+
+$('a').each(function(){
+  const url = $(this).attr('href');
+  $(this).parent().append(`(${url})`);
+});
